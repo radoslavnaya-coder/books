207340c16dff53ff7c4df0d7029ffc8b273f16bd
Radoslavna <73508256+radoslavnaya-coder@users.noreply.github.com>
2022-11-17 11:53:30 +0400

    Add files via upload

---
+++ books/main.js
@@ -0,0 +1,9 @@
+let clicker = document.getElementById("orange-clicker");
+let arr = [];
+let i = 0;
+clicker.forEach(function(item){
+    arr.push(item);
+})
+// clicker.addEventListener('click', function addcolor() {
+    
+// });
