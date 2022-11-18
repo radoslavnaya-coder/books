552a98ca56c52f96a923d2ebda678e9407cb4ec7
radoslavnaya-coder <killaarte13@gmail.com>
2022-11-18 21:56:34 +0400

    18.11 create

--- books/main.js
+++
@@ -1,9 +0,0 @@
-let clicker = document.getElementById("orange-clicker");
-let arr = [];
-let i = 0;
-clicker.forEach(function(item){
-    arr.push(item);
-})
-// clicker.addEventListener('click', function addcolor() {
-    
-// });
